dc34cc714808e7f96fa92fc5218ffb939a39a8e6
KaranKandhai <74590238+KaranKandhai@users.noreply.github.com>
2022-11-21 12:50:32 +0100

    Add files via upload

---
+++ if-sd-php-healthone/Templates/home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <?php
+    include_once('defaults/head.php');
+    ?>
+    <body>
+        <div class="container">
+            <?php
+            include_once ('defaults/header.php');
+            include_once ('defaults/menu.php');
+            include_once ('defaults/pictures.php');
+            ?>
+            <?php if(!empty($message)): ?>
+                <div class="alert alert-success" role="alert">
+                    <?=$message?>
+                </div>
+            <?php endif;?>
+            <h4>Epics Foodshop</h4>
+            Bij Epics Foodshop kan je eten en drinken bestellen en ook een review van geven.
+            <hr>
+            <?php
+            include_once ('defaults/footer.php');
+            ?>
+        </div>
+    </body>
+</html>
